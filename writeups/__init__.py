d58fdaea48aa39b3ca3c81bd07f353c5bee4f1a4
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-07-18 14:29:12 -0500

    Switch first writeup to markdown

--- writeups/__init__.py
+++ writeups/__init__.py
@@ -1,1 +1,20 @@
-import overflow1
+import overflow1, overflow2, overflow3, overflow4, overflow5
+from binaryninja import get_choice_input, PluginCommand
+
+choices = ['Overflow 1', 'Overflow 2', 'Overflow 3', 'Overflow 4', 'Overflow 5']
+
+def choose_writeup(bv):
+    choice = get_choice_input("Writeup:", "Open Writeup", choices) + 1
+
+    if choice == 1:
+        overflow1.render(bv)
+    elif choice == 2:
+        overflow2.render(bv)
+    elif choice == 3:
+        overflow3.render(bv)
+    elif choice == 4:
+        overflow4.render(bv)
+    elif choice == 5:
+        overflow5.render(bv)
+
+PluginCommand.register("Open Writeup", "Opens one of the PicoCTF Overflow Writeups", choose_writeup)
--- writeups/overflow1/__init__.py
+++ writeups/overflow1/__init__.py
@@ -1,13 +1,14 @@
-from binaryninja import show_html_report, show_plain_text_report
+from binaryninja import show_html_report, show_plain_text_report, show_markdown_report
 from mako.template import Template
 from mako import exceptions
-import os
+import os, glob
 
 name = os.path.dirname(os.path.realpath(__file__)).split('/')[-1]
-template = Template(filename=(os.path.dirname(os.path.realpath(__file__)) + '/writeup.mhtml'))
+template = Template(filename=(os.path.dirname(os.path.realpath(__file__)) + '/writeup.md'))
+images = list(glob.glob(os.path.dirname(os.path.realpath(__file__)) + '/images/*.png'))
 
 def render(bv):
     try:
-        show_html_report("{} Writeup".format(name), template.render(bv=bv))
+        show_markdown_report("{} Writeup".format(name), template.render(bv=bv, images=images))
     except:
         show_plain_text_report("{}: Error in Rendering".format(name), exceptions.text_error_template().render())
