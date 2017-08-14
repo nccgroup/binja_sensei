f244a8bbc881f95863dd7a40ea83f9f7ba30824d
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-08-14 11:00:26 -0500

    Fix typo in log statement

--- __init__.py
+++ __init__.py
@@ -14,14 +14,14 @@ def handle_dependencies(plugin):
             dependencies = raw_data["plugin"]["dependencies"]
             if "pip" in dependencies:
                 for package in dependencies["pip"]:
-                    print("Installing {} depdency: {}".format(plugin.name, package))
+                    print("Installing {} dependency: {}".format(plugin.name, package))
                     try:
                         pip.main(['install', '-q', package])
                     except IOError:
                         print("Unable to install {}. Permissions?".format(package))
                         traceback.print_exc()
     except IOError:
-        log_error("Unable to install dependencies for {}".format(plugin.name))
+        log_error("Unable to install dependencies for {}. Permissions?".format(plugin.name))
         traceback.print_exc()
 
 manager = RepositoryManager()
@@ -30,11 +30,11 @@ for plugin in manager.plugins['default']:
     if plugin.name in plugin_list:
         if not plugin.installed:
             log_info("Installing {}".format(plugin.name))
+            handle_dependencies(plugin)
             succ = manager.enable_plugin(plugin, install=True)
             if not succ:
                 log_error("{} installation failed".format(plugin.name))
                 traceback.print_exc()
-            handle_dependencies(plugin)
         else:
             log_info("Updating {}".format(plugin.name))
             manager.update_plugin(plugin)
